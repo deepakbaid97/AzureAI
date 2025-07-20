84b57119f76ad7741fc44a5908d687f062f29c07
Deepak <baid.deepak97@gmail.com>
2025-07-20 14:55:37 +0530

    Create Vision project

---
+++ AzureAIVision/AIVision/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace AzureAI.AzureAIVision.AIVision.Samples;
+
+public class Program
+{
+    public static void Main (string[] args)
+    {
+        Console.WriteLine("Hello WOrld");
+    }
+}
